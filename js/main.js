aeaaddeb750c6e56d365ae866dfe8b7edd1909b4
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2019-11-01 16:16:27 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+  
+function main() {
+
+    var config = {
+        type: Phaser.AUTO,
+        width: 1920,
+        height: 1080,
+        scene: {
+            create: create
+        }
+    };
+
+    var game = new Phaser.Game(config);
+}
+
+function create() {
+    var text = this.add.text(50, 50, "Phaser is working!", { fontSize: '32px', fill: '#FFF' });
+}
