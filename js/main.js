1cc5cd0450ee1a7736b82fbd1e8a10d93ff0449c
JayMassey98 <jaymassey98@gmail.com>
2019-11-10 00:03:45 +0000

    Changed screen size

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+/** @type {import("../typings/phaser")} */
+
 import { LoadScene } from "./scenes/LoadScene";
 import { MenuScene } from "./scenes/MenuScene";
 
 let game = new.Phaser.Game({
 
-    width: 1920,
-    height: 1080,
+    width: 800,
+    height: 600,
     scene: [
         LoadScene, MenuScene
     ],
