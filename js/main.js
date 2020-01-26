30fa75afd0128f01d0a83d1f2addbf0130250754
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-01-26 15:56:23 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,31 @@
 let game;
-let phaser;
 
-function main() {
+var gameSettings = {
     
-    var gameSettings = {
-    
-        playerSpeed: 200,
-        maxPowerUps: 2,
-        powerUpVelocity: 50
+    playerSpeed: 200,
+    maxPowerUps: 2,
+    powerUpVelocity: 50
 
-    }
-
-    var config = {
-
-        type: Phaser.AUTO,
-        width: 1920,
-        height: 937,
-        scene: [bootScene, playScene],
-        pixelArt: true,
-        
-        physics: {
-            
-            default: "arcade",
-            arcade: {
-                
-                debug: false,
-                debugShowVelocity: false
-
-            }
-        }
-    };
-
-    phaser = new Phaser.Game(config);
 }
+
+var config = {
+
+    // type: Phaser.AUTO,
+    width: 1920,
+    height: 937,
+    scene: [bootScene, playScene],
+    pixelArt: true,
+        
+    physics: {
+            
+        default: "arcade",
+        arcade: {
+                
+            debug: false,
+            debugShowVelocity: false
+            
+        }
+    }
+}
+
+game = new Phaser.Game(config);
