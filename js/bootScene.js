affda2bc8ff495f824804fabed40aba0a447b88a
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-01-26 22:15:12 +0000

    Update bootScene.js

--- js/bootScene.js
+++ js/bootScene.js
@@ -62,9 +62,6 @@ class bootScene extends Phaser.Scene {
     }
 
     create() {
-
-        beam = new Beam();
-        explosion = new Explosion();
         
         this.add.text(20, 20, "Loading...");
         this.scene.start("playGame");
