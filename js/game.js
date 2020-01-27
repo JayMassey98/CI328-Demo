6aecd52dc452bad4f3fbc5ff1f74bcd13ffda105
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-01-27 03:37:38 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 var config = {
 
     type: Phaser.AUTO,
-    width: 800,
-    height: 800,
+    width: 960,
+    height: 468,
     backgroundColor: 0x000000,
     scene: [bootScene, playScene],
     physics: {default: "arcade"},
