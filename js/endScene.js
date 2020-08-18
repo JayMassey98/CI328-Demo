9c83c5ff3467882fb80595a7a3aa0528543adb7a
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-08-18 21:16:29 +0100

    Update endScene.js

--- js/endScene.js
+++ js/endScene.js
@@ -10,29 +10,30 @@ class endScene extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.background.setOrigin(0, 0);
         
-        this.playButton = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, "buttonRestart");
-        this.playButton.setInteractive();
+        this.restartButton = this.add.sprite(config.width / 2, config.height / 2, "buttonRestart");
+        this.restartButton.setInteractive();
+        this.restartButton.scale.setTo(2, 2);
         
-        this.playButton.on("pointerup", function() {
-            this.scene.start("menu");
-        }, this);
+        this.restartButton.on("pointerup", function() { this.scene.start("menu"); }, this);
 
-        this.title = this.add.text(0,0, "Game Over.", {
+        this.title = this.add.text(0, 0, "Game Over.", {
             fontFamily: 'monospace',
             fontSize: 40,
             fontStyle: 'bold',
             color: '#ffffff',
             align: 'center'
         });
-        this.title.setOrigin(0,0);
+        
+        this.title.setOrigin(0, 0);
 
-        this.scoreText = this.add.text(config.width /2 , 0, "SCORE: 0", {
+        this.scoreText = this.add.text(config.width / 2 , 0, "SCORE: 0", {
             fontFamily: 'monospace',
             fontSize: 24,
             fontStyle: 'bold',
             color: '#ffffff',
             align: 'center'
         });
+        
         this.scoreText.setText("SCORE: " + this.score);
 
     }
