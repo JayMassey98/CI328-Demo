fc5ba4a5e315bd6fcbc311ebb6227a5f89386075
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-08-18 22:18:54 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -9,13 +9,16 @@ class menu extends Phaser.Scene {
 
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.background.setOrigin(0, 0);
+        
+        this.lightBeam = this.add.sprite(config.width / 2, config.height / 2 + 200, "lightBeam");
+        this.lightBeam.setScale(2, 2);
 
-        this.buttonPlay = this.add.sprite(config.width / 2, config.height / 2 - 100, "buttonPlay");
+        this.buttonPlay = this.add.sprite(config.width / 2, config.height / 2 + 200, "buttonPlay");
         this.buttonPlay.setInteractive();
         this.buttonPlay.setScale(2, 2);
         this.buttonPlay.on("pointerup", function() {this.scene.start("playScene"); }, this);
         
-        this.buttonOptions = this.add.sprite(config.width / 2, config.height / 2 + 100, "buttonOptions");
+        this.buttonOptions = this.add.sprite(config.width / 2, config.height / 2 + 300, "buttonOptions");
         this.buttonOptions.setInteractive();
         this.buttonOptions.setScale(2, 2);
         this.buttonOptions.on("pointerup", function() {this.scene.start("options"); }, this);
