281610d68d650844dd15e9fe31417074c392747d
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-08-18 15:10:54 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -10,29 +10,27 @@ class menu extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.background.setOrigin(0, 0);
 
-        this.buttonPlay = this.add.sprite(config.width / 2, config.height / 2, "button");
-        this.buttonOptions = this.add.sprite(config.width / 2, config.height / 3, "button");
+        this.buttonPlay = this.add.sprite(config.width / 2, config.height / 2, "buttonPlay");
+        this.buttonOptions = this.add.sprite(config.width / 2, config.height / 3, "buttonOptions");
 
         this.buttonPlay.setInteractive();
         this.buttonOptions.setInteractive();
 
-        this.title = this.add.text(0,0, "Light Beam", {
+        this.title = this.add.text(30, 30, "Light Beam", {
+            
             fontFamily: 'monospace',
             fontSize: 40,
             fontStyle: 'bold',
             color: '#ffffff',
             align: 'center'
+            
         });
         
-        this.title.setOrigin(0,0);
+        this.title.setOrigin(0, 0);
 
-        this.buttonPlay.on("pointerup", function() {
-            this.scene.start("playScene");
-        }, this);
+        this.buttonPlay.on("pointerup", function() {this.scene.start("playScene"); }, this);
 
-        this.buttonOptions.on("pointerup", function() {
-            this.scene.start("options");
-        }, this);
+        this.buttonOptions.on("pointerup", function() {this.scene.start("options"); }, this);
 
     }
 
