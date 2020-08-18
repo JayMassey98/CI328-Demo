8cf599491870b49e247844b966281a6a353c07d7
JayMassey98 <49314718+JayMassey98@users.noreply.github.com>
2020-08-18 14:20:14 +0100

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,39 @@
+class menu extends Phaser.Scene {
+    
+    constructor() {
+        
+        super({ key: "menu" });
+    }
+
+    create() {
+
+        this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
+        this.background.setOrigin(0, 0);
+
+        this.buttonPlay = this.add.sprite(config.width / 2, config.height / 2, "button");
+        this.option = this.add.sprite(config.width / 2, config.height / 3, "button");
+
+        this.buttonPlay.setInteractive();
+        this.option.setInteractive();
+
+        this.title = this.add.text(0,0, "Light Beam", {
+            fontFamily: 'monospace',
+            fontSize: 40,
+            fontStyle: 'bold',
+            color: '#ffffff',
+            align: 'center'
+        });
+        
+        this.title.setOrigin(0,0);
+
+        this.buttonPlay.on("pointerup", function() {
+            this.scene.start("playScene");
+        }, this);
+
+        this.option.on("pointerup", function() {
+            this.scene.start("options");
+        }, this);
+
+    }
+
+}
